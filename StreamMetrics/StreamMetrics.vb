e488bc6914b3d96262c94060c725f2b98f0ee648
Andrew Burnheimer <andrew.burnheimer@nbcuni.com>
2017-11-27 18:17:57 -0500

    Read from a pcap file, and report out the count of packets

--- StreamMetrics/StreamMetrics.vb
+++ StreamMetrics/StreamMetrics.vb
@@ -1,19 +1,26 @@
 ﻿Module StreamMetrics
+    Dim Count As Integer = 0
 
     Sub Main()
+
         Try
             Dim Filename As String = ""
             Dim args = My.Application.CommandLineArgs
             If args.Count() > 0 Then
-                Filename = args.Item(0).ToString
+                Filename = args.Item(args.Count() - 1).ToString
+                Console.Out().WriteLine("Reading: " & Filename)
             Else
                 Throw New MissingArgumentException("Please run on console with a filename argument, or drag and drop an input file on icon or shortcut.")
             End If
 
-            Dim ReadHandle As IO.StreamReader = FileIO.FileSystem.OpenTextFileReader(Filename)
-            'could be a big file...
-            Dim FileText As String = ReadHandle.ReadToEnd()
-            ReadHandle.Close()
+            Dim PacketDevice As PcapDotNet.Core.OfflinePacketDevice = New PcapDotNet.Core.OfflinePacketDevice(Filename)
+            Dim PacketCommunicator As PcapDotNet.Core.PacketCommunicator = PacketDevice.Open()
+
+            Using PacketCommunicator
+                PacketCommunicator.ReceivePackets(0, AddressOf DispatcherHandler)
+            End Using
+
+            Console.Out().WriteLine("Read " & Count & " packets")
 
         Catch e As MissingArgumentException
             Console.Out().WriteLine(e.Message)
@@ -23,6 +30,12 @@
         End Try
 
     End Sub
+
+    Sub DispatcherHandler(packet As PcapDotNet.Packets.Packet)
+        Count += 1
+        Console.Out().Write(".")
+    End Sub
+
     Class MissingArgumentException
         Inherits ConstraintException
         Public Sub New(s As String)
